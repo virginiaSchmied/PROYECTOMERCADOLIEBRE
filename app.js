f82a8e155d069e1e579683d04b9afd2225a13cee
virginiaSchmied <schmiedvirginia@gmail.com>
2023-01-04 15:53:46 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicFolderPath = path.resolve(__dirname, './public');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log('Servidor iniciado en http://localhost:'+ port);
+});
